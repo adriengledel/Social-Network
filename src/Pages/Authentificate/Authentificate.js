ba0cfaeb1cc8a3b234e2fc5743914a31e67267c0
Adrien <adriengledel@hotmail.fr>
2019-07-25 14:35:58 +0200

    auth page

--- src/Pages/Authentificate/Authentificate.js
+++ src/Pages/Authentificate/Authentificate.js
@@ -6,12 +6,35 @@ import LandingPage from 'components/common/LandingPage';
 
 import { LOGIN_PATH } from 'Routes/Paths';
 
+import { colors, typography } from 'styles';
+
+const Text = styled.div`
+  font-size : ${typography.xlarge}em;
+  color     : ${colors.blueElectron};
+`;
+
+const Content = styled.div`
+  display         : flex;
+  flex-direction  : column;
+  justify-content : center;
+  align-items     : center;
+  height          : 100%;
+`;
+
+const LoginPage = styled(Link)`
+  color      : ${colors.redElectron};
+  margin-top : 50px;
+  font-size  : ${typography.xlarge}em;
+`;
+
 class Authentificate extends React.Component{
   render(){
     return(
       <LandingPage>
-        <div>votre compte à bien été créé, vous avez reçu un mail</div>
-        <Link to="/">login page</Link>
+        <Content>
+          <Text>Félicitation votre compte à bien été créé ! vous recevrez un mail d'ici un instant</Text>
+          <LoginPage to="/">Page d'acceuil</LoginPage>
+        </Content>
       </LandingPage>
     );
   }
